8a7fcab59f0047572f320e216f9bc5b08f418cac
paul-rottger <eb@internetbande.de>
2021-03-02 18:20:50 +0000

    added module loading

--- 2_pretraining/pretrain_on_server.sh
+++ 2_pretraining/pretrain_on_server.sh
@@ -8,20 +8,27 @@
 # mail alert at start, end and abortion of execution
 #SBATCH --mail-type=ALL
 # send mail to this address
-#SBATCH --mail-user=paul.roettger@yahoo.de
+#SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
 #SBATCH --output=mlm.out
 #SBATCH --error=mlm.err
 
+# reset modules
+module purge
+
+# load python module
+module load python/anaconda3/2019.03
+
+# activate the right conda environment
+source activate $DATA/conda-envs/gab-language-change
+
 python run_mlm.py \
-    --model_name_or_path bert-base-uncased \
+    --model_name_or_path $DATA/bert-base-uncased \
     --train_file ../0_data/clean/train.txt \
     --validation_file ../0_data/clean/eval.txt \
-    --use_special_tokens \
-    --cache_dir $DATA/huggingface-default-models \
     --line_by_line \
     --do_train \
     --do_eval \
-    --output_dir ./test-mlm \
+    --output_dir $DATA/adapted-models/test-mlm \
     --overwrite_output_dir \
     --num_train_epochs 1 \
     --max_seq_length 64
