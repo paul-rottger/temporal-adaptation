ea0558150754b3235515af2af68f0d8226c97be6
paul-rottger <eb@internetbande.de>
2021-03-02 14:25:30 +0000

    first ARC trial

---
+++ 2_pretraining/pretrain_model.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Executing the pretraining script with set options
+python run_mlm.py \
+    --model_name_or_path bert-base-uncased \
+    --train_file ../0_data/clean/train.txt \
+    --validation_file ../0_data/clean/eval.txt \
+    --use_special_tokens \
+    --line_by_line \
+    --do_train \
+    --do_eval \
+    --output_dir ./test-mlm \
+    --overwrite_output_dir \
+    --num_train_epochs 1 \
+    --max_seq_length 64
