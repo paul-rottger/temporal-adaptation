9f3f7b6c2029c00353946715ac0f2a151f006904
paul-rottger <paul.roettger@yahoo.de>
2021-03-03 11:55:01 +0000

    added ARC commands

--- 2_pretraining/pretrain_on_server.sh
+++ 2_pretraining/pretrain_on_server.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-# set the number of nodes
+
 #SBATCH --nodes=1
-# set max wallclock time
 #SBATCH --time=10:00:00
-# set name of job
 #SBATCH --job-name=test123
-# mail alert at start, end and abortion of execution
 #SBATCH --mail-type=ALL
-# send mail to this address
 #SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
 #SBATCH --output=mlm.out
 #SBATCH --error=mlm.err
