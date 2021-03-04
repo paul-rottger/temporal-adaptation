41b2362450e5803e1a6aa20d6a4e003ad8eea550
paul-rottger <paul.roettger@yahoo.de>
2021-03-04 12:06:01 +0000

    cleaned and split labelled data

--- 3_finetuning/finetune_on_local.sh
+++ 3_finetuning/finetune_on_local.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# Executing the pretraining script with set options
-python run_mlm.py \
-    --model_name_or_path ../0_models/default-model \
+# Executing the finetuning script with set options
+python run_finetuning.py \
+    --model_name_or_path ../0_models/bert-random-1m-3ep \
     --train_file ../0_data/clean/train.txt \
     --validation_file ../0_data/clean/eval.txt \
     --do_train \
     --do_eval \
-    --output_dir ./test-mlm \
+    --output_dir ../0_models/test-mlm \
     --overwrite_output_dir \
     --num_train_epochs 1 \
     --max_seq_length 64
