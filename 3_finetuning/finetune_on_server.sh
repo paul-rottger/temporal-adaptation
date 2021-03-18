6880fce34d32e7fa8830913f01228f91d9bd49e7
paul-rottger <paul.roettger@yahoo.de>
2021-03-18 16:53:30 +0000

    shifted finetuning

--- 3_finetuning/finetune_on_server.sh
+++ 3_finetuning/finetune_on_server.sh
@@ -25,17 +25,18 @@ nvidia-smi
 
 # Executing the finetuning script with set options
 #for i in 01 02 03 04 05 06 07 08 09 10; do
-for modelpath in $DATA/gab-language-change/adapted-models/month-models/*/; do
+#for modelpath in $DATA/gab-language-change/adapted-models/month-models/*/; do
+for ((i=1; i<=8; i++)); do
 
     python run_finetuning.py \
-        --model_name_or_path $modelpath \
-        --train_file $DATA/gab-language-change/0_data/clean/labelled_gq/train_random.csv \
-        --validation_file $DATA/gab-language-change/0_data/clean/labelled_gq/eval_random.csv \
+        --model_name_or_path $DATA/gab-language-change/adapted-models/month-models/bert-0$((i+1))_1m \
+        --train_file $DATA/gab-language-change/0_data/clean/labelled_ghc/month_splits/train-0$i.csv \
+        --validation_file $DATA/gab-language-change/0_data/clean/labelled_ghc/month_splits/test-0$i.csv \
         --do_train \
         --per_device_train_batch_size 32 \
         --do_eval \
         --per_device_eval_batch_size 128 \
-        --output_dir $DATA/gab-language-change/finetuned-models/gq/$(basename $modelpath)-train-gq \
+        --output_dir $DATA/gab-language-change/finetuned-models/ghc/month-models/bert-0$((i+1))-1m-train-0$i \
         --overwrite_output_dir \
         --num_train_epochs 3 \
         --max_seq_length 128 \
--- 4_evaluation/classification/test_on_server.sh
+++ 4_evaluation/classification/test_on_server.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
 #SBATCH --output=test.out
 #SBATCH --error=test.err
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:k80:1
 
 # reset modules
 module purge
@@ -24,14 +24,14 @@ nvidia-smi
 #
 
 # Executing the finetuning script with set options
-for modelpath in $DATA/gab-language-change/finetuned-models/bert-*/; do
+for modelpath in $DATA/gab-language-change/finetuned-models/gq/*/; do
 
     python run_test.py \
         --model_name_or_path $modelpath \
-        --test_file $DATA/gab-language-change/0_data/clean/labelled_ghc/eval_random.csv \
-        --per_device_eval_batch_size 512 \
+        --test_file $DATA/gab-language-change/0_data/clean/labelled_gq/eval_random.csv \
+        --per_device_eval_batch_size 256 \
         --output_dir $DATA/gab-language-change/eval-results/predictions/classification \
-        --output_name $(basename $modelpath) \
+        --output_name $(basename $modelpath)-test-gq \
         --overwrite_output_dir \
         --max_seq_length 128 \
         --use_special_tokens
