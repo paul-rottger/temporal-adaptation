b5a426d13d5e3b2cd3f695ac4eafe4690cf84feb
paul-rottger <paul.roettger@yahoo.de>
2021-03-31 20:08:04 +0100

    pretrain rand 10m reddit

--- 2_pretraining/pretrain_on_server.sh
+++ 2_pretraining/pretrain_on_server.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #SBATCH --partition=htc
-#SBATCH --time=24:00:00
-#SBATCH --job-name=17-08-pretrain
+#SBATCH --time=48:00:00
+#SBATCH --job-name=10m-rand-pretrain
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
-#SBATCH --output=17-08-pretrain.out
-#SBATCH --error=17-08-pretrain.err
-#SBATCH --gres=gpu:v100:1
+#SBATCH --output=10m-rand-pretrain.out
+#SBATCH --error=10m-rand-pretrain.err
+#SBATCH --gres=gpu:1 --constraint='gpu_mem:32GB'
 
 # reset modules
 module purge
@@ -25,18 +25,18 @@ nvidia-smi
 
 python run_mlm.py \
     --model_name_or_path $DATA/gab-language-change/default-models/bert-base-uncased \
-    --train_file $DATA/gab-language-change/0_data/clean/unlabelled_reddit/month_splits/train_2017_08_1m.txt \
-    --validation_file $DATA/gab-language-change/0_data/clean/unlabelled_reddit/month_splits/test_2017_01_10k.txt \
+    --train_file $DATA/gab-language-change/0_data/clean/unlabelled_reddit/total/train_rand_10m.txt \
+    --validation_file $DATA/gab-language-change/0_data/clean/unlabelled_reddit/total/test_rand_10k.txt \
     --save_steps 20000 \
     --use_special_tokens \
     --line_by_line \
     --do_train \
-    --per_device_train_batch_size 64 \
+    --per_device_train_batch_size 128 \
     --do_eval \
     --per_device_eval_batch_size 128 \
     --evaluation_strategy epoch \
     --dataset_cache_dir $DATA/gab-language-change/z_cache/datasets \
-    --output_dir $DATA/gab-language-change/adapted-models/reddit/month-models/bert-2017_08_1m \
+    --output_dir $DATA/gab-language-change/adapted-models/reddit/total-models/bert-rand_10m \
     --overwrite_output_dir \
     --num_train_epochs 1 \
     --max_seq_length 128
