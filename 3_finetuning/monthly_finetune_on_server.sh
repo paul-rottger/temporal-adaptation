d9a5a90cdbc8a8e62574d864759b43efb003cdfd
paul-rottger <paul.roettger@yahoo.de>
2021-04-05 13:57:32 +0100

    ada rand finetune with 2k

--- 3_finetuning/monthly_finetune_on_server.sh
+++ 3_finetuning/monthly_finetune_on_server.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --partition=htc
 #SBATCH --time=24:00:00
-#SBATCH --job-name=ra-m17-finetune
+#SBATCH --job-name=ada-rand-finetune
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
-#SBATCH --output=ra-m17-finetune-finetune.out
-#SBATCH --error=ra-m17-finetune.err
-#SBATCH --gres=gpu:v100:1
+#SBATCH --output=ada-rand-finetune.out
+#SBATCH --error=ada-rand-finetune.err
+#SBATCH --gres=gpu:k80:1
 
 # reset modules
 module purge
@@ -25,19 +25,21 @@ nvidia-smi
 
 
 # Finetune the rand_1m model on each month-set of training data
-for trainpath in $DATA/gab-language-change/0_data/clean/labelled_reddit/month_splits/train_2017*_20k.csv; do
-    
-    python run_finetuning.py \
-        --model_name_or_path $DATA/gab-language-change/adapted-models/reddit/total-models/bert-rand_1m \
-        --train_file $trainpath \
-        --validation_file $trainpath \
-        --do_train \
-        --per_device_train_batch_size 32 \
-        --output_dir $DATA/gab-language-change/finetuned-models/reddit/month-models/ada-rand/bert-rand_1m-$(basename $trainpath .csv) \
-        --overwrite_output_dir \
-        --dataset_cache_dir $DATA/gab-language-change/z_cache/datasets \
-        --num_train_epochs 3 \
-        --max_seq_length 128 \
-        --use_special_tokens
+for modelpath in $DATA/gab-language-change/adapted-models/reddit/total-models/bert-rand_1m/; do
+    for trainpath in $DATA/gab-language-change/0_data/clean/labelled_reddit/month_splits/train_*_2k.csv; do
+        
+        python run_finetuning.py \
+            --model_name_or_path $modelpath \
+            --train_file $trainpath \
+            --validation_file $trainpath \
+            --do_train \
+            --per_device_train_batch_size 32 \
+            --output_dir $DATA/gab-language-change/finetuned-models/reddit/month-models/ada-rand/$(basename $modelpath)-$(basename $trainpath .csv) \
+            --overwrite_output_dir \
+            --dataset_cache_dir $DATA/gab-language-change/z_cache/datasets \
+            --num_train_epochs 3 \
+            --max_seq_length 128 \
+            --use_special_tokens
 
+    done
 done
