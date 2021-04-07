5d6dc7900f754d98102d07a57193208b5dfadfc9
paul-rottger <paul.roettger@yahoo.de>
2021-04-07 14:37:08 +0100

    ghc-rand-test

--- 4_evaluation/classification/test_on_server.sh
+++ 4_evaluation/classification/test_on_server.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --partition=htc
 #SBATCH --time=24:00:00
-#SBATCH --job-name=eval
+#SBATCH --job-name=ghc-rand-test
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
-#SBATCH --output=test.out
-#SBATCH --error=test.err
-#SBATCH --gres=gpu:v100:1
+#SBATCH --output=ghc-rand-test.out
+#SBATCH --error=ghc-rand-test.err
+#SBATCH --gres=gpu:k80:1
 
 # reset modules
 module purge
@@ -24,14 +24,14 @@ nvidia-smi
 #
 
 # Executing the finetuning script with set options
-for modelpath in $DATA/gab-language-change/finetuned-models/reddit/total-models/bert-rand_10m-*/; do
+for modelpath in $DATA/gab-language-change/finetuned-models/ghc/total-models/bert*/; do
 
     python run_test.py \
         --model_name_or_path $modelpath \
-        --test_file $DATA/gab-language-change/0_data/clean/labelled_reddit/total/test_rand_10k.csv \
+        --test_file $DATA/gab-language-change/0_data/clean/labelled_ghc/total/test_rand_4k.csv \
         --per_device_eval_batch_size 256 \
-        --output_dir $DATA/gab-language-change/eval-results/reddit/classification/total-models \
-        --output_name $(basename $modelpath)-test_rand_10k \
+        --output_dir $DATA/gab-language-change/eval-results/classification/ghc/total-models \
+        --output_name $(basename $modelpath)-test_rand_4k \
         --overwrite_output_dir \
         --max_seq_length 128 \
         --use_special_tokens
