7c4f9af6efba841fe3f43bd5c3024c1f1532d963
paul-rottger <paul.roettger@yahoo.de>
2021-04-13 17:28:30 +0100

    next stage of cumu pretraining

--- 2_pretraining/cumu_pretrain_on_server.sh
+++ 2_pretraining/cumu_pretrain_on_server.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --partition=htc
 #SBATCH --time=24:00:00
-#SBATCH --job-name=17-cumu-pretrain
+#SBATCH --job-name=18-cumu-pretrain
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=paul.rottger@oii.ox.ac.uk
-#SBATCH --output=17-cumu-pretrain.out
-#SBATCH --error=17-cumu-pretrain.err
+#SBATCH --output=18-cumu-pretrain.out
+#SBATCH --error=18-cumu-pretrain.err
 #SBATCH --gres=gpu:v100:1
 
 # reset modules
@@ -23,14 +23,50 @@ source activate $DATA/conda-envs/gab-language-change
 nvidia-smi
 #
 
+# 17/11 --> 17/12
+
+python run_mlm.py \
+    --model_name_or_path $DATA/gab-language-change/adapted-models/reddit/cumu-models/bert-cumu_2017_11_1m \
+    --train_file $DATA/gab-language-change/0_data/clean/unlabelled_reddit/month_splits/train_2017_12_1m.txt \
+    --validation_file $DATA/gab-language-change/0_data/clean/unlabelled_reddit/total/test_rand_10k.txt \
+    --save_steps 20000 \
+    --use_special_tokens \
+    --line_by_line \
+    --do_train \
+    --per_device_train_batch_size 64 \
+    --dataset_cache_dir $DATA/gab-language-change/z_cache/datasets \
+    --output_dir $DATA/gab-language-change/adapted-models/reddit/cumu-models/bert-cumu_2017_12_1m \
+    --overwrite_output_dir \
+    --num_train_epochs 1 \
+    --max_seq_length 128
+
+sleep 15s
+
+# 17/12 --> 18/01
+
+python run_mlm.py \
+    --model_name_or_path $DATA/gab-language-change/adapted-models/reddit/cumu-models/bert-cumu_2017_12_1m \
+    --train_file $DATA/gab-language-change/0_data/clean/unlabelled_reddit/month_splits/train_2018_01_1m.txt \
+    --validation_file $DATA/gab-language-change/0_data/clean/unlabelled_reddit/total/test_rand_10k.txt \
+    --save_steps 20000 \
+    --use_special_tokens \
+    --line_by_line \
+    --do_train \
+    --per_device_train_batch_size 64 \
+    --dataset_cache_dir $DATA/gab-language-change/z_cache/datasets \
+    --output_dir $DATA/gab-language-change/adapted-models/reddit/cumu-models/bert-cumu_2018_01_1m \
+    --overwrite_output_dir \
+    --num_train_epochs 1 \
+    --max_seq_length 128
+
 
 # set year for which to adapt models (run separately and subsequently for subsequent years)
-year="2017"
+year="2018"
 
 # set month to load initial model from
-load_month="03"
+load_month="01"
 
-for train_month in "04" "05" "06" "07" "08" "09" "10" "11" "12"; do
+for train_month in "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12"; do
     
     echo "loading model from:" $load_month $year
     echo "adapting it to data from:" $train_month $year
